80f70be0fbfab561d37202f54f1528d4b2523bab
Kiss Bence <kissbence326@gmail.com>
2024-02-20 08:39:26 +0100

    remove getData

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,10 @@
 import Image from "next/image";
 
-const getData = async () => {
+export default async function Home() {
   const res = await fetch("http://localhost:3000/api/hello", {
     cache: "no-store",
   });
-
-  if (!res.ok) {
-    throw new Error("Hello error");
-  }
-
-  return res.json();
-};
-
-export default async function Home() {
-  const data = await getData();
+  const data = res.json();
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
