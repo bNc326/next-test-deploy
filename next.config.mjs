dcecd69154496f8d8a58c488aaa390aa87d307fa
Kiss Bence <kissbence326@gmail.com>
2024-02-20 09:13:15 +0100

    vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  
+  env: {
+    NEXT_PUBLIC_API: process.env.NEXT_PUBLIC_API,
+  },
 };
 
 export default nextConfig;
