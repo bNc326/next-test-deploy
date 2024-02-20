bb8be4fc26f2b0bbb70ab1278f2f6d38d82e2087
Kiss Bence <kissbence326@gmail.com>
2024-02-20 08:42:21 +0100

    add revalidate

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default async function Home() {
   const res = await fetch("http://localhost:3000/api/hello", {
-    cache: "no-store",
+    next: { revalidate: 3600 },
   });
   const data = res.json();
   return (
