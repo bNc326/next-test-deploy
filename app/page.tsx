373a325f4e120490b2be3af85c095835380391bc
Kiss Bence <kissbence326@gmail.com>
2024-02-20 08:55:54 +0100

    fetch

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
+import { Joan } from "next/font/google";
 import Image from "next/image";
 
 const dynamic = "force-dynamic";
 
 export default async function Home() {
-  const res = await fetch("http://localhost:3000/api/hello", {
+  const res = await fetch(process.env.NEXT_PUBLIC_API as string, {
     cache: "no-store",
   });
-  const data = res.json();
+  const data = await res.json();
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
